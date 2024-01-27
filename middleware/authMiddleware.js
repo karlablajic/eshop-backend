8ad034b8cd988403a86af30e4e23b3e8dbded88c
karlablajic <100536593+karlablajic@users.noreply.github.com>
2024-01-27 12:49:38 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,26 @@
 const jwt = require('jsonwebtoken');
 
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
 
-//check current user
+    if(token) {
+        jwt.verify(token, 'eshop', async (err, decodedToken) => {
+            if(err) {
+                console.log(err.message);
+                res.direct('/login');
+            } else{
+                console.log(decodedToken);
+                next();
+            }
+        })
+    }
+    else {
+        res.redirekt('/login')
+    }
+}
+
+
+
 const checkUser = (req, res, next) => {
     const token = req.cookies.jwt;
 
