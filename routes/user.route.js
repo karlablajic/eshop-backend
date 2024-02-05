1112f674db325e2a206d733f38fb227ed5310ace
karlablajic <100536593+karlablajic@users.noreply.github.com>
2024-02-05 14:51:26 +0100

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,47 +1,99 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
+const authenticateToken = require('../middleware/authMiddleware');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const jwtSecret = '4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd'
 
 
-const maxAge = 3*24*60*60;
-      const createToken = (id) => {
-        return jwt.sign({ id }, 'eshop', {
-          expiresIn
-        })
-      }
-router.post('/signup', async(req, res)=> {
-    const {name, email, password} = req.body;
 
-    try {
-        // Assuming 'name', 'email', and 'password' are variables containing user input
-        const user = await User.create({ name, email, password });
-        const token = createToken(user_.id)
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 * 60 * 60 * 24 });
-        res.json({user: user_.id});
-
-      } catch (error) {
-        // Check if the error is a duplicate key violation (code 11000 for MongoDB)
-        if (error.code === 11000) {
-          return res.status(400).send('Email already exists');
-        }
-      
-        // Handle other validation errors or unexpected errors
-        if (error.name === 'ValidationError') {
-          // If it's a validation error, you can extract specific error messages
-          const validationErrors = Object.values(error.errors).map((err) => err.message);
-          return res.status(400).json({ errors: validationErrors });
-        }
-      
-        // For unexpected errors, send a generic error message
-        res.status(500).send('Internal Server Error');
-      }
-    });
-
-    router.post('/login', async (req, res) => {
-        const { email, password } = req.body;
+router.post('/signup', async (req, res) => {
+  try {
+      const { name, email, password } = req.body;
     
+   
+     const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+          name,
+          email,
+          password: hashedPassword,
+      });
+
+      const maxAge = 3 * 60 * 60; 
+
+      const token = jwt.sign(
+          { id: user._id, username: user.name, role: user.role },
+          jwtSecret,
+          {
+              expiresIn: maxAge, 
+          }
+      );
+
+      res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: maxAge * 1000,
+      });
+
+
+      res.status(201).json({
+          message: "User successfully created",
+          user: user._id,
+          token: token,
+      });
+  } catch (error) {
+      
+      res.status(400).json({
+          message: "User not successfully created",
+          error: error.message,
+      });
+  }
+});
+
+      
+      router.post('/login', async (req, res) => {
         try {
+            const { email, password } = req.body;
+    
+            const user = await User.findOne({ email });
+    
+            if (!user) {
+                return res.status(401).json({ message: "Authentication failed. User not found." });
+            }
+    
+            const passwordMatch = await bcrypt.compare(password, user.password);
+    
+            if (!passwordMatch) {
+                return res.status(401).json({ message: "Authentication failed. Invalid password." });
+            }
+    
+            const token = jwt.sign(
+                { id: user._id, email: user.email, role: user.role },
+                jwtSecret,
+                { expiresIn: '1h' } 
+            );
+    
+            res.cookie('jwt', token, {
+                httpOnly: true,
+                maxAge: 3600000,
+            });
+    
+    
+            res.status(200).json({
+                message: 'Authentication successful',
+                token: token,
+            });
+        } catch (error) {
+            
+            console.error(error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    });
+    
+      
+    
+        /*try {
           // Use a meaningful function name for better readability
           const authenticatedUser = await User.authenticate(email, password);
           res.json(authenticatedUser);
@@ -50,55 +102,67 @@ router.post('/signup', async(req, res)=> {
           if (error.name === 'AuthenticationError') {
             return res.status(401).send('Invalid email or password');
           }}});
-   
+   */
       
-      router.get('/', async (req, res) => {
-        try {
-          // Use a more specific query and meaningful variable names for better readability
-          const regularUsers = await User.find({ isAdmin: false }).populate('orders');
-      
-          // Return the list of regular users along with their orders
-          res.json(regularUsers);
-        } catch (error) {
-          // Handle specific error cases
-          if (error.name === 'ValidationError') {
-            // If it's a validation error, you can extract specific error messages
-            const validationErrors = Object.values(error.errors).map((err) => err.message);
-            return res.status(400).json({ errors: validationErrors });
-          }
-      
-          // For unexpected errors, send a generic error message
-          res.status(500).send('Internal Server Error');
-        }
-      });
-      
+          router.get('/', authenticateToken, async (req, res) => {
+            try {
+                // Ensure that the user is an admin
+                if (req.user.role !== 'admin') {
+                    return res.status(403).json({ message: 'Forbidden: You are not authorized to access this resource.' });
+                }
+        
+                // Query the database to find regular users
+                const regularUsers = await User.find({ isAdmin: false }).populate('orders');
+        
+                // Return the list of regular users along with their orders
+                res.json(regularUsers);
+            } catch (error) {
+                // Handle specific error cases
+                if (error.name === 'ValidationError') {
+                    // If it's a validation error, you can extract specific error messages
+                    const validationErrors = Object.values(error.errors).map((err) => err.message);
+                    return res.status(400).json({ errors: validationErrors });
+                }
+        
+                // For unexpected errors, send a generic error message
+                res.status(500).send('Internal Server Error');
+            }
+        });
+        
 
-      router.get('/:id/orders', async (req, res) => {
-        const { id } = req.params;
-      
-        try {
-          // Use meaningful variable names for better readability
-          const foundUser = await User.findById(id).populate('orders');
-      
-          if (!foundUser) {
+// Route to get orders for a specific user
+router.get('/:id/orders', authenticateToken, async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const foundUser = await User.findById(id).populate('orders');
+
+        if (!foundUser) {
             return res.status(404).send('User not found');
-          }
-      
-          // Return the orders associated with the user
-          res.json(foundUser.orders);
-        } catch (error) {
-          // Handle specific error cases
-          if (error.name === 'CastError') {
-            return res.status(400).send('Invalid user ID');
-          }
-      
-          // For unexpected errors, send a generic error message
-          res.status(500).send('Internal Server Error');
         }
-      });
+
+        // Check if the user has admin role
+        if (req.user.role !== 'admin') {
+            return res.status(403).send('Forbidden: You are not authorized to access this resource');
+        }
+
+        res.json(foundUser.orders);
+    } catch (error) {
+        if (error.name === 'CastError') {
+            return res.status(400).send('Invalid user ID');
+        }
+
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
     
-      module.exports = router;
+ module.exports = router;
+      
+
+
+ 
       
 
 
