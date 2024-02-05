3b2a2e5afd3f06d23343b0d3261e6d66689a030e
karlablajic <100536593+karlablajic@users.noreply.github.com>
2024-02-05 14:50:14 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,46 +1,22 @@
 const jwt = require('jsonwebtoken');
+const jwtSecret = '4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd'
 
-const requireAuth = (req, res, next) => {
+// Middleware function to authenticate JWT token
+const authenticateToken = (req, res, next) => {
     const token = req.cookies.jwt;
-
-    if(token) {
-        jwt.verify(token, 'eshop', async (err, decodedToken) => {
-            if(err) {
-                console.log(err.message);
-                res.direct('/login');
-            } else{
-                console.log(decodedToken);
-                next();
-            }
-        })
+  
+    if (token == null) {
+        return res.status(401).send('Unauthorized: No token provided');
     }
-    else {
-        res.redirekt('/login')
-    }
-}
-
-
-
-const checkUser = (req, res, next) => {
-    const token = req.cookies.jwt;
-
-    if(token) {
-        jwt.verify(token, 'eshop', async (err, decodedToken) => {
-            if(err) {
-                console.log(err.message);
-                res.locals.user = null;
-                next();
-            } else{
-                console.log(decodedToken);
-                let user = await User.findById(decodedToken.id);
-                res.locals.user = user;
-                next();
-            }
-        })
-    }
-    else {
-        res.locals.user = null;
+  
+    jwt.verify(token, jwtSecret, (err, user) => {
+        if (err) {
+            return res.status(403).send('Forbidden: Invalid token');
+        }
+        req.user = user;
         next();
-    }
-}
-module.exports = { requireAuth, checkUser};
+    });
+    return;
+  };
+
+  module.exports = authenticateToken;
